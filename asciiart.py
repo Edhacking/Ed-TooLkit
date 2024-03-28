2b9f4c3b4ec0729866a5d44b6e59107d0c2a044b
Edhacking <165077294+Edhacking@users.noreply.github.com>
2024-03-29 02:31:08 +0300

    Update asciiart.py

--- asciiart.py
+++ asciiart.py
@@ -6,7 +6,7 @@ ascii_art = ('''
 ███████╗██████╔╝       ██║   ╚██████╔╝╚██████╔╝███████╗███████║
 ╚══════╝╚═════╝        ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
                                                                 Version:3.1
-✔ Author : MR.Ed  .  
-✔ Coded By : Abdizamed moha 
+✔ Author : MR.Ed                                 .  
+✔ Coded By : Abdizamed moha                      .
 ✔ GitHub : https://github.com/Edhacking/Ed-TooLkit           
   ''')
